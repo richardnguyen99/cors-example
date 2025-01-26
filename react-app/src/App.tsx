143f62f18c00d80635aa8efc91605d2a0a025b95
Richard Nguyen <richard@richardhnguyen.com>
2025-01-25 19:08:12 -0800

    add preflight requests

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
 import "./App.css";
 
 const BASE_API_URL = "http://localhost:8989/api";
@@ -7,6 +7,7 @@ function App() {
   const [response, setResponse] = useState({});
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<Error | null>(null);
+  const [inputValue, setInputValue] = useState("");
 
   const fetchWithCORS = async () => {
     setLoading(true);
@@ -53,6 +54,34 @@ function App() {
     setLoading(false);
   };
 
+  const postWithCORS = useCallback(async () => {
+    setLoading(true);
+
+    try {
+      const resp = await fetch(`${BASE_API_URL}/with-cors`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ title: `Learn ${inputValue}` }),
+      });
+      const data = await resp.json();
+
+      setResponse(data);
+      setError(null);
+    } catch (err) {
+      setResponse({});
+      setError(err as Error);
+    }
+
+    setLoading(false);
+    setInputValue("");
+  }, [inputValue]);
+
+  const handleInputValue = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -69,9 +98,19 @@ function App() {
         <button type="button" onClick={fetchWithoutCORS}>
           Without CORS
         </button>
+        <button type="button" onClick={postWithCORS}>
+          Post CORS
+        </button>
       </div>
 
       <div className="mt-4">
+        <div className="my-3">
+          <input
+            className="w-full px-4 py-2 rounded-lg bg-gray-800 ring-0 border border-gray-700 focus-visible:ring-gray-700 outline-none"
+            value={inputValue}
+            onChange={handleInputValue}
+          ></input>
+        </div>
         {loading && <p>Loading...</p>}
         {response && (
           <pre className="bg-gray-800 text-white p-4 mb-4 rounded-lg text-left">
--- rest-app/src/routes/api.js
+++ rest-app/src/routes/api.js
@@ -3,6 +3,14 @@ const cors = require("cors");
 
 const apiRouter = express.Router();
 
+const data = {
+  todos: [
+    { id: 1, title: "Learn React" },
+    { id: 2, title: "Learn Node" },
+    { id: 3, title: "Learn Express" },
+  ],
+};
+
 apiRouter.get("/", (req, res) => {
   res.json({
     message: "Hello from the API",
@@ -13,37 +21,39 @@ apiRouter.get("/with-cors", (req, res) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("X-CORS-Policy", "CORS-Manual");
 
-  res.json({
-    todos: [
-      { id: 1, title: "Learn React" },
-      { id: 2, title: "Learn Node" },
-      { id: 3, title: "Learn Express" },
-    ],
-  });
+  res.json(data);
+});
+
+apiRouter.options("/with-cors", (req, res) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, HEAD");
+
+  res.send();
+});
+
+apiRouter.post("/with-cors", (req, res) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("X-CORS-Policy", "CORS-Manual");
+
+  data.todos.push({
+    id: data.todos.length + 1,
+    title: req.body.title,
+  })
+
+  res.json(data);
 });
 
 apiRouter.get("/without-cors", (req, res) => {
   res.header("X-CORS-Policy", "No-CORS");
 
-  res.json({
-    todos: [
-      { id: 1, title: "Learn React" },
-      { id: 2, title: "Learn Node" },
-      { id: 3, title: "Learn Express" },
-    ],
-  });
+  res.json(data);
 });
 
 apiRouter.get("/with-cors-plugin", cors(), (req, res) => {
   res.header("X-CORS-Policy", "CORS-Plugin");
 
-  res.json({
-    todos: [
-      { id: 1, title: "Learn React" },
-      { id: 2, title: "Learn Node" },
-      { id: 3, title: "Learn Express" },
-    ],
-  });
+  res.json(data);
 });
 
 module.exports = apiRouter;
